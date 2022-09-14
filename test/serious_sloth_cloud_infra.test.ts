2bed52fcea7b2bd7bf5173985f14e83b08644b92
Aaron <aaronkikabhai97@gmail.com>
2022-09-14 16:08:22 +0100

    Initial commit

---
+++ test/serious_sloth_cloud_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SeriousSlothCloudInfra from '../lib/serious_sloth_cloud_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serious_sloth_cloud_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SeriousSlothCloudInfra.SeriousSlothCloudInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
